c95e9785e4df9749975c41c976a473ed5051e777
Asher <asher.bergtraun@gmail.com>
2021-05-30 22:31:35 -0700

    javascript global variables established

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,22 @@
-//API key: cf589712e83c2302d49522e11784d1ab
+//API Key from Open Weather API
+var APIkey = "cf589712e83c2302d49522e11784d1ab";
+
+//global variables for Today Card
+var City = "";
+var todayDate = "";
+var todayIconCode = "";
+var todayIconUrl = "";
+var todayTemp = "";
+var todayWind = "";
+var todayHumidity = "";
+var UVindex = "";
+
+//global variables for forecast Cards
+var forecastTemp = "";
+var forecastHumidity = "";
+var forecasticoncode = "";
+var forecasticonurl = "";
+
+//global variables
+var latitude = "";
+var longitude = "";
