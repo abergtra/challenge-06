aa1de31b439109d128ef104e032208056ff744a2
Asher <asher.bergtraun@gmail.com>
2021-05-31 08:30:22 -0700

    create searched cities array on search button click

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -31,10 +31,55 @@ if (localStorageCities !== null) {
 
 //Identify current searched city from array and run search function
 $(document).ready(function(){
-    displayCities(searchedCities);
+    showCityList(searchedCities);
     if (localStorageCities !== null) {
       var currentCity = searchedCities[0];
       searchCity(currentCity);
     }
 });
 
+//function to Clear Active Display and global variables
+function clearDisplay () {
+    //Clear active display
+    $("#current-weather").empty();
+    $("#card-deck-title").remove();
+    $(".card-deck").empty();
+    //Reset global variables
+    City = "";
+    todayDate = "";
+    todayIconCode = "";
+    todayIconUrl = "";
+    todayTemp = "";
+    todayWind = "";
+    todayHumidity = "";
+    UVindex = "";
+    forecastTemp = "";
+    forecastHumidity = "";
+    forecasticoncode = "";
+    forecasticonurl = "";
+    latitude = "";
+    longitude = "";
+}
+
+//Function response to Search Button click
+$("#search-button").on("click", function() {
+    event.preventDefault();
+    clearDisplay();
+    //get User Input city name and search
+    var cityName = $("input").val().toUpperCase().trim();
+    $("#city-input").val("");
+    searchCity(cityName);
+    
+    //build searchedCities array
+    if (cityName !== "" && searchedCities[0] !== cityName) {
+      searchedCities.unshift(cityName);
+      localStorage.setItem("city-search-history", JSON.stringify(searchedCities));
+      //if searchedCities array has content display info
+      if (searchedCities.length === 1) {
+        $("#city-list").removeClass("hide");
+      }
+      //display list of searched cities
+      $("#city-list").prepend(`<a href="#" class="list-group-item btn-block" style="text-decoration: none; color: black; background-color: #b7b7b7; border: none; text-align: center;">
+      ${cityName}</a>`);
+    }
+  });
