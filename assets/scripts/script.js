9c42300d8177d162ed7f64d7e8675f7758fe2fb0
Asher <asher.bergtraun@gmail.com>
2021-05-31 22:22:25 -0700

    Search City function structure established

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -67,6 +67,7 @@ $("#search-button").on("click", function() {
     clearDisplay();
     //get User Input city name and search
     var cityName = $("input").val().toUpperCase().trim();
+    console.log("City Searched: ",cityName);
     $("#city-input").val("");
     searchCity(cityName);
     
@@ -82,4 +83,73 @@ $("#search-button").on("click", function() {
       $("#city-list").prepend(`<a href="#" class="list-group-item btn-block" style="text-decoration: none; color: black; background-color: #b7b7b7; border: none; text-align: center;">
       ${cityName}</a>`);
     }
-  });
+});
+
+//Function response to clicking previously searched City Name
+$(document).on("click", ".list-group-item", function() {
+    var cityName = $(this).text();
+    clearDisplay();
+    searchCity(cityName);
+});
+
+//Function to search info for a city
+function searchCity(cityName){
+    console.log("searchCity: " , cityName);
+    // Define API url to locate info
+    var APIurl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+
+    // Perform an asynchornous HTTP (ajax) request to the OpenWeatherAPI
+    $.ajax({url: APIurl, method: "GET"})
+
+    // store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+        var result = response;
+        console.log(result);
+        city = result.name.trim();
+        //  var countryCode = result.sys.country;
+        //  country = getCountryName(countryCode).trim();
+        //  currentDate = moment().tz(country + "/" + city).format('l');
+        currentDate = moment.unix(result.dt).format("l");
+        console.log(currentDate);
+        var tempK = result.main.temp;
+        // Converts the temp to Kelvin with the below formula
+        tempF = ((tempK - 273.15) * 1.80 + 32).toFixed(1);
+        humidityValue = result.main.humidity;
+        windSpeed = result.wind.speed;
+        currentWeatherIconCode = result.weather[0].icon;
+        currentWeatherIconUrl = "https://openweathermap.org/img/w/" + currentWeatherIconCode + ".png";
+        var latitude = result.coord.lat;
+        var longitude = result.coord.lon;
+        var uvIndexQueryUrl = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude;
+        $.ajax({
+            url: uvIndexQueryUrl,
+            method: "GET"
+        })
+        .then(function(response) {
+            uvIndexValue = response.value;
+            displayCurrentWeather()
+                
+            var fiveDayQueryUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&appid=" + APIKey + "&cnt=5";
+            $.ajax({
+                url: fiveDayQueryUrl,
+                method: "GET"
+            })
+            .then(function(response) {
+                var fiveDayForecast = response.list;
+                addCardDeckHeader()
+                for (var i=0; i < 5; i++) {
+                    iconcode = fiveDayForecast[i].weather[0].icon;
+                    iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+                    //  dateValue = moment().tz(country + "/" + city).add(i, 'days').format('l');
+                    dateValue = moment.unix(fiveDayForecast[i].dt).format('l');
+                    minTempK = fiveDayForecast[i].temp.min;
+                    minTempF =  ((minTempK - 273.15) * 1.80 + 32).toFixed(1);
+                    maxTempK = fiveDayForecast[i].temp.max;
+                    maxTempF =  (((fiveDayForecast[i].temp.max) - 273.15) * 1.80 + 32).toFixed(1);
+                    dayhumidity = fiveDayForecast[i].humidity;
+                    displayDayForeCast()
+                } 
+            });      
+        }); 
+    });
+}
